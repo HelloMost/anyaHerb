2ffc5befb11311e55fc7b89c80dea794fe1eda7c
HelloMost <mostiere@hotmail.com>
2024-02-23 22:03:10 +0700

    commit
    clean note in .js

--- custom.js
+++ custom.js
@@ -1,15 +1,3 @@
-//1.
-document.addEventListener("DOMContentLoaded", function () {
-    
-    setTimeout(function () {
-        document.querySelector("h1").classList.add("fade-in");
-        document.querySelector("p").classList.add("fade-in");
-    }, 500);
-});
-
-
-//2.
-// Function to check if an element is in the viewport
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
     return (
@@ -18,7 +6,7 @@ function isInViewport(element) {
     );
 }
 
-// Function to animate the picture when it's in the viewport
+//capsule
 function animateCapsule() {
     const picture = document.querySelector('.capsule');
     
@@ -28,40 +16,33 @@ function animateCapsule() {
     }
 }
 
-// Attach a scroll event listener to trigger the animation
+
 window.addEventListener('scroll', animateCapsule);
 
-// Initially trigger the animation if the picture is in the viewport when the page loads
 document.addEventListener('DOMContentLoaded', animateCapsule);
 
 
 
 
-// Function to apply the scaling effect when the picture is in the viewport
 function scalePicture() {
     const picture = document.querySelector('.capsule');
     const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
     
-    // Calculate the position of the picture relative to the viewport
     const rect = picture.getBoundingClientRect();
     const distanceFromTop = rect.top;
     
-    // Calculate the scaling factor based on the distance from the top
     const scaleFactor = 1 + (distanceFromTop / viewportHeight);
     
     
-    // Apply the scaling effect to the picture
     picture.style.transform = `scale(${scaleFactor})`;
 }
 
-// Attach a scroll event listener to trigger the scaling effect
 window.addEventListener('scroll', scalePicture);
 
-// Initial trigger for the scaling effect
 document.addEventListener('DOMContentLoaded', scalePicture);
 
 
-//flower//
+//flower
 
 function animateFlower() {
     const picture1 = document.querySelector('.flower');
@@ -83,19 +64,14 @@ function scalePicture1() {
     const picture1 = document.querySelector('.flower');
     const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
     
-    // Calculate the position of the picture relative to the viewport
     const rect = picture1.getBoundingClientRect();
     const distanceFromTop = rect.top;
     
-    // Calculate the scaling factor based on the distance from the top
     const scaleFactor = 1 + (distanceFromTop / viewportHeight);
     
-    // Apply the scaling effect to the picture
     picture1.style.transform = `scale(${scaleFactor})`;
 }
 
-// Attach a scroll event listener to trigger the scaling effect
 window.addEventListener('scroll', scalePicture1);
 
-// Initial trigger for the scaling effect
 document.addEventListener('DOMContentLoaded', scalePicture1);
